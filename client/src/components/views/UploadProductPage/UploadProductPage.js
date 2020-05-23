e77672e76cac92532ed54031cec739def89a3202
morbi <the_morbidus@hotmail.com>
2020-05-23 00:33:39 -0400

    built uploadproductpage form submit stage 1

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 function UploadProductPage() {
   return (
-    <div>
+    <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
+      <div style={{ textAlign:'center', marginBottom:'2rem'}}>
+        <h2> Upload Travel Product</h2>
+      </div>
       
+      <form onSubmit >
+
+        {/* DropZone */}
+
+        <br />
+        <br />
+        <input 
+          onChange
+          value
+        />
+        <br />
+        <br />
+
+      </form>
     </div>
   )
 }
